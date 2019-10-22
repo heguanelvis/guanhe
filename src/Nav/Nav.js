9e13fb6ce2c774ba706220f73d0a28bfcc389fca
Guan He <heguanelvis@gmail.com>
2019-10-22 14:58:15 -0400

    added side navbar

--- src/Nav/Nav.js
+++ src/Nav/Nav.js
@@ -4,29 +4,30 @@ import "./Nav.scss";
 
 export default function Nav() {
     return (
-        <div className="nav-container">
-            <nav>
-                <div className="nav-brand">
-                    <NavLink to="#">
+        <nav className="nav-container">
+            <ul>
+                <li>
+                    {" "}
+                    <NavLink className="nav-brand__link" to="#">
                         <img className="nav-brand__img" src="./favicon.ico" />
                     </NavLink>
-                    <h1>Guan He</h1>
-                </div>
-                <ul>
-                    <li>
-                        <NavLink to="#">About</NavLink>
-                    </li>
-                    <li>
-                        <NavLink to="#">Portfolio</NavLink>
-                    </li>
-                    <li>
-                        <NavLink to="#">Resume</NavLink>
-                    </li>
-                    <li>
-                        <NavLink to="#">Contact</NavLink>
-                    </li>
-                </ul>
-            </nav>
-        </div>
+                </li>
+                <li>
+                    <NavLink to="#">Guan He</NavLink>
+                </li>
+                <li>
+                    <NavLink to="#">About</NavLink>
+                </li>
+                <li>
+                    <NavLink to="#">Portfolio</NavLink>
+                </li>
+                <li>
+                    <NavLink to="#">Resume</NavLink>
+                </li>
+                <li>
+                    <NavLink to="#">Contact</NavLink>
+                </li>
+            </ul>
+        </nav>
     );
 }
