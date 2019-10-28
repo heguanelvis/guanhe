36c16896f6e555fc7f2bbda8bcc8d1b6b61421a4
Guan He <heguanelvis@gmail.com>
2019-10-27 23:50:39 -0400

    added about section

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,5 +1,9 @@
 import React from "react";
 import "./About.scss";
+import img1 from "./1.jpg";
+import img2 from "./2.jpg";
+import img3 from "./3.jpg";
+import img4 from "./4.jpg";
 
 export default function About() {
     return (
@@ -9,136 +13,54 @@ export default function About() {
             </h1>
 
             <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer
-                interdum varius est eget porttitor. Suspendisse consectetur
-                ultricies nibh, vestibulum egestas leo molestie sit amet.
-                Phasellus ultricies nibh augue, ut placerat sem mattis sed.
-                Integer in sodales sapien. Integer facilisis tempus diam, ac
-                tempor risus eleifend nec. Sed vel lacus eget ligula blandit
-                tincidunt quis sollicitudin magna. Aenean velit dui, rutrum
-                vitae vestibulum non, lobortis sit amet arcu. Ut rhoncus in nisl
-                sed vulputate. Nam ipsum neque, volutpat porta est non, volutpat
-                rhoncus mi. In est tellus, lobortis at metus nec, egestas
-                finibus tellus. Praesent venenatis tellus metus, quis tempus
-                mauris tristique vel. Nullam finibus rhoncus tempus. Suspendisse
-                scelerisque sit amet felis in gravida. Sed dictum efficitur
-                mollis. Donec lobortis sem eget nibh tincidunt, nec rutrum eros
-                condimentum. Sed volutpat volutpat erat ac fringilla. Duis
-                ornare tincidunt justo a dictum. Sed at tincidunt orci, eget
-                rutrum nibh. Nunc ac felis ac arcu auctor ullamcorper. Fusce
-                rutrum massa sit amet maximus posuere. Praesent aliquam purus
-                semper tempor aliquet. Fusce lobortis quis sem quis tincidunt.
-                Fusce pulvinar mollis lacus, sit amet viverra dui. Quisque porta
-                vitae mi eget placerat. Aenean ac elementum libero. Vestibulum
-                ante ipsum primis in faucibus orci luctus et ultrices posuere
-                cubilia Curae; Pellentesque ultrices vitae dolor et ultricies.
-                Ut interdum cursus lectus et viverra. Praesent placerat sapien
-                eget nibh scelerisque laoreet sed at eros. Vivamus hendrerit
-                purus faucibus tortor condimentum, non tristique est blandit.
-                Donec laoreet dui ac massa vulputate, non varius risus varius.
-                Nunc ac neque in magna fringilla tempus eget eget enim. Sed quis
-                velit in dui sagittis finibus et et eros. Suspendisse lobortis
-                lacus et pharetra gravida. Curabitur sagittis, lectus in blandit
-                ornare, dui ex laoreet odio, a interdum odio orci id elit.
-                Aenean volutpat congue luctus. Vivamus efficitur tortor non
-                molestie finibus. Aliquam erat volutpat. Vivamus placerat, est
-                vitae aliquet sagittis, lectus turpis maximus dolor, sit amet
-                vulputate tortor arcu efficitur turpis. Morbi vitae lorem in
-                tortor varius imperdiet in a neque. Aliquam eu turpis nisl.
-                Integer vestibulum ullamcorper lacus, sed faucibus odio aliquam
-                id. Ut interdum vel libero in scelerisque. Aliquam fringilla et
-                turpis vitae volutpat. Suspendisse ullamcorper in leo nec
-                vehicula. Aenean tempus erat ipsum, sit amet tristique neque
-                venenatis a. Nulla tincidunt lectus eget luctus imperdiet.
-                Vestibulum id bibendum felis. Donec et felis porttitor, rutrum
-                dui et, rhoncus lorem. Proin placerat lacinia libero quis
-                pulvinar. Etiam porta id enim ut finibus. Phasellus eu aliquet
-                eros, sed porta nunc. Sed eleifend est suscipit nisl laoreet
-                eleifend. Morbi pretium sapien non pharetra fermentum. Curabitur
-                massa diam, suscipit at bibendum sed, finibus et libero. Nulla
-                viverra gravida venenatis. Nulla pharetra nunc non sodales
-                cursus. Sed id dolor et ante porta lobortis vel sit amet massa.
-                Phasellus sit amet eleifend lectus, id suscipit dolor. Lorem
-                ipsum dolor sit amet, consectetur adipiscing elit. Integer
-                interdum varius est eget porttitor. Suspendisse consectetur
-                ultricies nibh, vestibulum egestas leo molestie sit amet.
-                Phasellus ultricies nibh augue, ut placerat sem mattis sed.
-                Integer in sodales sapien. Integer facilisis tempus diam, ac
-                tempor risus eleifend nec. Sed vel lacus eget ligula blandit
-                tincidunt quis sollicitudin magna. Aenean velit dui, rutrum
-                vitae vestibulum non, lobortis sit amet arcu. Ut rhoncus in nisl
-                sed vulputate. Nam ipsum neque, volutpat porta est non, volutpat
-                rhoncus mi. In est tellus, lobortis at metus nec, egestas
-                finibus tellus. Praesent venenatis tellus metus, quis tempus
-                mauris tristique vel. Nullam finibus rhoncus tempus. Suspendisse
-                scelerisque sit amet felis in gravida. Sed dictum efficitur
-                mollis. Donec lobortis sem eget nibh tincidunt, nec rutrum eros
-                condimentum. Sed volutpat volutpat erat ac fringilla. Duis
-                ornare tincidunt justo a dictum. Sed at tincidunt orci, eget
-                rutrum nibh. Nunc ac felis ac arcu auctor ullamcorper. Fusce
-                rutrum massa sit amet maximus posuere. Praesent aliquam purus
-                semper tempor aliquet. Fusce lobortis quis sem quis tincidunt.
-                Fusce pulvinar mollis lacus, sit amet viverra dui. Quisque porta
-                vitae mi eget placerat. Aenean ac elementum libero. Vestibulum
-                ante ipsum primis in faucibus orci luctus et ultrices posuere
-                cubilia Curae; Pellentesque ultrices vitae dolor et ultricies.
-                Ut interdum cursus lectus et viverra. Praesent placerat sapien
-                eget nibh scelerisque laoreet sed at eros. Vivamus hendrerit
-                purus faucibus tortor condimentum, non tristique est blandit.
-                Donec laoreet dui ac massa vulputate, non varius risus varius.
-                Nunc ac neque in magna fringilla tempus eget eget enim. Sed quis
-                velit in dui sagittis finibus et et eros. Suspendisse lobortis
-                lacus et pharetra gravida. Curabitur sagittis, lectus in blandit
-                ornare, dui ex laoreet odio, a interdum odio orci id elit.
-                Aenean volutpat congue luctus. Vivamus efficitur tortor non
-                molestie finibus. Aliquam erat volutpat. Vivamus placerat, est
-                vitae aliquet sagittis, lectus turpis maximus dolor, sit amet
-                vulputate tortor arcu efficitur turpis. Morbi vitae lorem in
-                tortor varius imperdiet in a neque. Aliquam eu turpis nisl.
-                Integer vestibulum ullamcorper lacus, sed faucibus odio aliquam
-                id. Ut interdum vel libero in scelerisque. Aliquam fringilla et
-                turpis vitae volutpat. Suspendisse ullamcorper in leo nec
-                vehicula. Aenean tempus erat ipsum, sit amet tristique neque
-                venenatis a. Nulla tincidunt lectus eget luctus imperdiet.
-                Vestibulum id bibendum felis. Donec et felis porttitor, rutrum
-                dui et, rhoncus lorem. Proin placerat lacinia libero quis
-                pulvinar. Etiam porta id enim ut finibus. Phasellus eu aliquet
-                eros, sed porta nunc. Sed eleifend est suscipit nisl laoreet
-                eleifend. Morbi pretium sapien non pharetra fermentum. Curabitur
-                massa diam, suscipit at bibendum sed, finibus et libero. Nulla
-                viverra gravida venenatis. Nulla pharetra nunc non sodales
-                cursus. Sed id dolor et ante porta lobortis vel sit amet massa.
-                Phasellus sit amet eleifend lectus, id suscipit dolor. Lorem
-                ipsum dolor sit amet, consectetur adipiscing elit. Integer
-                interdum varius est eget porttitor. Suspendisse consectetur
-                ultricies nibh, vestibulum egestas leo molestie sit amet.
-                Phasellus ultricies nibh augue, ut placerat sem mattis sed.
-                Integer in sodales sapien. Integer facilisis tempus diam, ac
-                tempor risus eleifend nec. Sed vel lacus eget ligula blandit
-                tincidunt quis sollicitudin magna. Aenean velit dui, rutrum
-                vitae vestibulum non, lobortis sit amet arcu. Ut rhoncus in nisl
-                sed vulputate. Nam ipsum neque, volutpat porta est non, volutpat
-                rhoncus mi. In est tellus, lobortis at metus nec, egestas
-                finibus tellus. Praesent venenatis tellus metus, quis tempus
-                mauris tristique vel. Nullam finibus rhoncus tempus. Suspendisse
-                scelerisque sit amet felis in gravida. Sed dictum efficitur
-                mollis. Donec lobortis sem eget nibh tincidunt, nec rutrum eros
-                condimentum. Sed volutpat volutpat erat ac fringilla. Duis
-                ornare tincidunt justo a dictum. Sed at tincidunt orci, eget
-                rutrum nibh. Nunc ac felis ac arcu auctor ullamcorper. Fusce
-                rutrum massa sit amet maximus posuere. Praesent aliquam purus
-                semper tempor aliquet. Fusce lobortis quis sem quis tincidunt.
-                Fusce pulvinar mollis lacus, sit amet viverra dui. Quisque porta
-                vitae mi eget placerat. Aenean ac elementum libero. Vestibulum
-                ante ipsum primis in faucibus orci luctus et ultrices posuere
-                cubilia Curae; Pellentesque ultrices vitae dolor et ultricies.
-                Ut interdum cursus lectus et viverra. Praesent placerat sapien
-                eget nibh scelerisque laoreet sed at eros. Vivamus hendrerit
-                purus faucibus tortor condimentum, non tristique est blandit.
-                Donec laoreet dui ac massa vulputate, non varius risus varius.
-                Nunc ac neque in magna fringilla tempus eget eget enim. Sed quis
-                velit in dui
+                Welcome to my website! My name is Guan He. I am a
+                twenty-four-year old curious individual who loves coding and
+                solving challenges.
             </p>
+            <p>
+                I grew up in China, and had my BA in International Studies,
+                focusing on comparative politics and governance practices. In
+                2017, I attended the University of Chicago, where I learned data
+                analytics and visualization, specifically in the context of
+                making policies. At the same time, I took multiple civic
+                technology courses and developed an enthusiasm for programming.
+                In the summer of 2019, I graduated with a Master of Public
+                Policy and decided that I want to further my skills and pursue a
+                career in technology. Currently, I am a full-time Java student
+                at Tech Elevator learning about Test Driven Development and the
+                Spring Framework.
+            </p>
+            <p>
+                {" "}
+                I have contributed to several small data-driven web projects
+                featuring my personal interests and civic technology during the
+                past two years. I specifically enjoy using the MERN Stack to
+                build websites and using D3.js to create interactive and mobile
+                responsive visualizations. I love collaborating with my team
+                members and I am always willing to learn more and take on new
+                challenges.{" "}
+            </p>
+            <p>
+                In my spare time, I like cooking, singing, swimming, playing
+                video games, taking care of my plants and spending time with my
+                cat. I enjoy talking with all kinds of people and look forward
+                to making more friends in the Detroit area.
+            </p>
+
+            <div className="about-img-wrapper">
+                <div>
+                    <img src={img1} alt="cat1" />
+                </div>
+                <div>
+                    <img src={img2} alt="me1" />
+                </div>
+                <div>
+                    <img src={img3} alt="me2" />
+                </div>
+                <div>
+                    <img src={img4} alt="me3" />
+                </div>
+            </div>
         </div>
     );
 }
