f86e5912a0b4892f7c122e28341036ea04090bf7
heguanelvis <heguanelvis@gmail.com>
2019-11-03 16:43:10 -0500

    fixed small style issues and classname error

--- src/components/Photos/Photos.js
+++ src/components/Photos/Photos.js
@@ -7,32 +7,32 @@ import photos from "./life-img.json";
 
 const SortablePhoto = SortableElement(item => <Photo {...item} />);
 const SortableGallery = SortableContainer(({ items }) => (
-    <Gallery
-        photos={items}
-        direction="column"
-        columns={columns}
-        renderImage={props => <SortablePhoto {...props} />}
-    />
+  <Gallery
+    photos={items}
+    direction="column"
+    columns={columns}
+    renderImage={props => <SortablePhoto {...props} />}
+  />
 ));
 
 export default function Photos() {
-    const [items, setItems] = useState(photos);
+  const [items, setItems] = useState(photos);
 
-    const onSortEnd = ({ oldIndex, newIndex }) => {
-        setItems(arrayMove(items, oldIndex, newIndex));
-    };
+  const onSortEnd = ({ oldIndex, newIndex }) => {
+    setItems(arrayMove(items, oldIndex, newIndex));
+  };
 
-    return (
-        <div class="photos-div">
-            <SortableGallery items={items} onSortEnd={onSortEnd} axis={"xy"} />
-        </div>
-    );
+  return (
+    <div className="photos-div">
+      <SortableGallery items={items} onSortEnd={onSortEnd} axis={"xy"} />
+    </div>
+  );
 }
 
 function columns(containerWidth) {
-    let columns = 2;
-    if (containerWidth >= 500) columns = 4;
-    if (containerWidth >= 700) columns = 5;
-    if (containerWidth >= 1000) columns = 7;
-    return columns;
+  let columns = 2;
+  if (containerWidth >= 500) columns = 4;
+  if (containerWidth >= 700) columns = 5;
+  if (containerWidth >= 1000) columns = 7;
+  return columns;
 }
